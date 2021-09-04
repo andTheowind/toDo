dc84e19ca29d75bf437a7ef3c98e9293a0853479
andTheowind <59265638+andTheowind@users.noreply.github.com>
2021-09-04 23:27:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -141,28 +141,7 @@ colorSelection.addEventListener('click', function () {
 
 });
 
-const buttonClickEffect = (e) => {
-    const x = e.clientX;
-    const y = e.clientY;
 
-    const buttonTop = e.target.offsetTop;
-    const buttonLeft = e.target.offsetLeft;
-
-    const xInside = x - buttonLeft;
-    const yInside = y - buttonTop;
-
-    const circle = document.createElement('span');
-    circle.classList.add('circle');
-    circle.style.top = yInside + 'px';
-    circle.style.left = xInside + 'px';
-
-    console.log(xInside, yInside);
-
-    this.appendChild(circle);
-
-    setTimeout(() => circle.remove(), 500);
-}
 btn.addEventListener("click", () => { 
     addListItem();
-    buttonClickEffect();
 })
